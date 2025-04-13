19290451ccdbb5457449af495b0c55738032a8f6
mdyeasinhn <mdyeasinhn@gmail.com>
2025-04-13 19:39:38 +0600

    Formatting Response Using the sendResponse Function

--- server/src/app/modules/Admin/admin.controller.ts
+++ server/src/app/modules/Admin/admin.controller.ts
@@ -1,125 +1,128 @@
-
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterAbleFileds } from "./admin.constent";
+import sendResponse from "../../../shared/sendResponse";
 
 
 
 const getAllAdminFromDB = async (req: Request, res: Response) => {
+  const filters = pick(req.query, adminFilterAbleFileds);
+  const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder']);
 
+  try {
+    const result = await adminService.getAllAdminFromDB(filters, options);
 
-    const filters = pick(req.query, adminFilterAbleFileds)
-    const options = pick(req.query, ['page', 'limit', 'sortBy', 'sortOrder']);
-    console.log('options', options)
-    try {
-        const result = await adminService.getAllAdminFromDB(filters, options);
-
-        res.status(200).json({
-            success: true,
-            message: "Retrieving All Admin Data from the Database",
-            meta: result.meta,
-            data: result
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong!",
-            error: err
-        })
-    }
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Retrieving All Admin Data from the Database",
+      meta: result.meta,
+      data: result,
+    });
+  } catch (err: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: err?.message || "Something went wrong!",
+      data: null,
+    });
+  }
 };
-
-
 
 const getByIdFromDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
-    try {
-        const result = await adminService.getByIdFromDB(id)
+  try {
+    const result = await adminService.getByIdFromDB(id);
 
-        res.status(200).json({
-            success: true,
-            message: "Admin data fateched by id!",
-            data: result
-        })
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong!",
-            error: err
-        })
-    }
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admin data fetched by ID!",
+      data: result,
+    });
+  } catch (err: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: err?.message || "Something went wrong!",
+      data: null,
+    });
+  }
 };
-
 
 const updatedIntoDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
 
-    const { id } = req.params;
-    const data = req.body;  
-    try {
-        const result = await adminService.updatedIntoDB(id, data)
+  try {
+    const result = await adminService.updatedIntoDB(id, data);
 
-        res.status(200).json({
-            success: true,
-            message: "Admin data updated!",
-            data: result
-        })
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong!",
-            error: err
-        })
-    }
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admin data updated!",
+      data: result,
+    });
+  } catch (err: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: err?.message || "Something went wrong!",
+      data: null,
+    });
+  }
 };
 
-
 const deleteIntoDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
-    try {
-        const result = await adminService.deleteFromDB(id)
+  try {
+    const result = await adminService.deleteFromDB(id);
 
-        res.status(200).json({
-            success: true,
-            message: "Admin data deleted!",
-            data: result
-        })
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong!",
-            error: err
-        })
-    }
-}
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admin data deleted!",
+      data: result,
+    });
+  } catch (err: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: err?.message || "Something went wrong!",
+      data: null,
+    });
+  }
+};
+
 const softDeleteIntoDB = async (req: Request, res: Response) => {
+  const { id } = req.params;
 
-    const { id } = req.params;
-    try {
-        const result = await adminService.softDeleteFromDB(id)
+  try {
+    const result = await adminService.softDeleteFromDB(id);
 
-        res.status(200).json({
-            success: true,
-            message: "Admin data deleted!",
-            data: result
-        })
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err?.name || "Something went wrong!",
-            error: err
-        })
-    }
-}
-
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Admin data soft-deleted!",
+      data: result,
+    });
+  } catch (err: any) {
+    sendResponse(res, {
+      statusCode: 500,
+      success: false,
+      message: err?.message || "Something went wrong!",
+      data: null,
+    });
+  }
+};
 
 export const adminController = {
-    getAllAdminFromDB,
-    getByIdFromDB,
-    updatedIntoDB,
-    deleteIntoDB,
-    softDeleteIntoDB
-}
-
+  getAllAdminFromDB,
+  getByIdFromDB,
+  updatedIntoDB,
+  deleteIntoDB,
+  softDeleteIntoDB,
+};
