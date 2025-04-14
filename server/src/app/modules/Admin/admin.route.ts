254383537b4bf58e97bc165fc2ef27e3f53ae5de
mdyeasinhn <mdyeasinhn@gmail.com>
2025-04-14 12:05:39 +0600

    Implementing admin validation in update admin

--- server/src/app/modules/Admin/admin.route.ts
+++ server/src/app/modules/Admin/admin.route.ts
@@ -1,23 +1,8 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import { adminController } from './admin.controller';
-import { AnyZodObject, z } from 'zod';
+import validateRequest from '../../middlewares/validateRequest';
+import { adminValidation } from './admin.validation';
 
-const updateAdminSchema = z.object({
-    boyd: z.object({
-        name: z.string().optional(),
-        contactNumber: z.string().optional()
-    })
-})
-
-const validateRequest = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        await schema.parseAsync({
-            body : req.body
-        })
-    } catch (err) {
-        next(err)
-    }
-}
 
 
 const router = express.Router();
@@ -26,9 +11,10 @@ router.get("/", adminController.getAllAdminFromDB);
 
 router.get("/:id", adminController.getByIdFromDB);
 
-router.patch("/:id", validateRequest(updateAdminSchema), adminController.updatedIntoDB);
+router.patch("/:id", validateRequest(adminValidation.updateAdminSchema), adminController.updatedIntoDB);
 
 router.delete("/:id", adminController.deleteIntoDB);
+
 router.delete("/soft/:id", adminController.softDeleteIntoDB);
 
 export const adminRoutes = router;
